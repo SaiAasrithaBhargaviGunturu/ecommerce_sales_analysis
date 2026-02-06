a89158a961ec499a156d3af79d17525808e94e40
SaiAasrithaBhargaviGunturu <yshuashu4@gmail.com>
2026-02-05 20:25:39 -0600

    Add sales and profit by region query

--- sql/sql_queries.sql
+++ sql/sql_queries.sql
@@ -14,3 +14,13 @@ SELECT
 FROM ecommerce_sales_data
 GROUP BY Category
 ORDER BY total_profit DESC;
+-- Business Question 3:
+-- Sales and Profit by Region
+
+SELECT
+    Region,
+    SUM(Sales) AS total_sales,
+    SUM(Profit) AS total_profit
+FROM ecommerce_sales_data
+GROUP BY Region
+ORDER BY total_profit DESC;
